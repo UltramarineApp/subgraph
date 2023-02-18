eb0301432f928f8cc4142f1fd5ab3ffd32379b09
Alex Baker <alex.baker.fon@gmail.com>
2023-02-19 00:23:38 +0200

    Ultramarine Subgraph Factory

--- src/ultra.ts
+++ src/ultra.ts
@@ -1,21 +1,21 @@
-import { NewGame as NewGameEvent } from "../generated/Ultra/Ultra"
-import { NewGame } from "../generated/schema"
+import { NewGame as NewGameEvent } from "../generated/Ultra/Ultra";
+import { NewGame } from "../generated/schema";
 
 export function handleNewGame(event: NewGameEvent): void {
   let entity = new NewGame(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id_ = event.params.id_
-  entity.game_ = event.params.game_
-  entity.name_ = event.params.name_
-  entity.background1_ = event.params.background1_
-  entity.emoji1_ = event.params.emoji1_
-  entity.background2_ = event.params.background2_
-  entity.emoji2_ = event.params.emoji2_
+  );
+  entity.num_ = event.params.id_;
+  entity.game_ = event.params.game_;
+  entity.name_ = event.params.name_.toString();
+  entity.background1_ = event.params.background1_;
+  entity.emoji1_ = event.params.emoji1_;
+  entity.background2_ = event.params.background2_;
+  entity.emoji2_ = event.params.emoji2_;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
--- tests/ultra-utils.ts
+++ tests/ultra-utils.ts
@@ -1,9 +1,9 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
-import { NewGame } from "../generated/Ultra/Ultra"
+import { newMockEvent } from "matchstick-as";
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts";
+import { NewGame } from "../generated/Ultra/Ultra";
 
 export function createNewGameEvent(
-  id_: BigInt,
+  num_: BigInt,
   game_: Address,
   name_: string,
   background1_: string,
@@ -11,37 +11,37 @@ export function createNewGameEvent(
   background2_: string,
   emoji2_: string
 ): NewGame {
-  let newGameEvent = changetype<NewGame>(newMockEvent())
+  let newGameEvent = changetype<NewGame>(newMockEvent());
 
-  newGameEvent.parameters = new Array()
+  newGameEvent.parameters = new Array();
 
   newGameEvent.parameters.push(
     new ethereum.EventParam("id_", ethereum.Value.fromUnsignedBigInt(id_))
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam("game_", ethereum.Value.fromAddress(game_))
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam("name_", ethereum.Value.fromString(name_))
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam(
       "background1_",
       ethereum.Value.fromString(background1_)
     )
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam("emoji1_", ethereum.Value.fromString(emoji1_))
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam(
       "background2_",
       ethereum.Value.fromString(background2_)
     )
-  )
+  );
   newGameEvent.parameters.push(
     new ethereum.EventParam("emoji2_", ethereum.Value.fromString(emoji2_))
-  )
+  );
 
-  return newGameEvent
+  return newGameEvent;
 }
